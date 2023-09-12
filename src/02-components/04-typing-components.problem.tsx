f3ff6460b4391bbacaa4aac06389bb683d1fec2e
Sam <lenry101@gmail.com>
2023-09-12 21:43:23 +0100

    exercise 04 amend with explainer

--- src/02-components/04-typing-components.problem.tsx
+++ src/02-components/04-typing-components.problem.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export const Button = (props: {className: string}) => {
-  return <button className={props.className}></button>;
+// here we add inline type definition for our props
+// we can also extract the types to an interface or type
+export const Button = ({className}: {className: string}) => {
+  return <button className={className}></button>;
 };
 
 const Parent = () => {
