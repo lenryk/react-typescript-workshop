c0afad653f233c7058916db24cd38d4165538314
Sam <lenry101@gmail.com>
2023-09-13 22:03:25 +0100

    exercise 05

--- src/02-components/05-typing-components-as-functions.problem.tsx
+++ src/02-components/05-typing-components-as-functions.problem.tsx
@@ -4,8 +4,10 @@ interface Props {
   className: string;
 }
 
+// we need to grab a type definition from React to use for our function component
+// we get it off the React object
 /* @ts-expect-error */
-export const Button = (props: Props) => {
+export const Button: React.FC<Props> = (props: Props) => {
   return {
     ohDear: "123",
   };
