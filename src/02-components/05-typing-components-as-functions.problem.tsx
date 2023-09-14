904e29e3e7046a2e14bca62ad8cb0086381b18d6
Sam <lenry101@gmail.com>
2023-09-14 22:48:31 +0100

    exercise 05 amend desc

--- src/02-components/05-typing-components-as-functions.problem.tsx
+++ src/02-components/05-typing-components-as-functions.problem.tsx
@@ -7,6 +7,7 @@ interface Props {
 // we need to grab a type definition from React to use for our function component
 // we get it off the React object
 // we pass the props arg to let it know what props it is expecting
+// this is from legacy code - in modern code it will be props: PropsInterface
 /* @ts-expect-error */
 export const Button: React.FC<Props> = (props: Props) => {
   return {
