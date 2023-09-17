e1e06637236f2ee16dd0e5ef175d663cddaebf39
Sam <lenry101@gmail.com>
2023-09-17 23:53:44 +0100

    exercise 20

--- src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
+++ src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 
+// when we pass null it returns a RefObject which is readonly
+// you can read this in the type definitions (click with null to see which function overload it goes to)
+// when we remove the initial value it becomes MutableRefObject<>
 export const Component = () => {
-  const ref = useRef<string>(null);
+  const ref = useRef<string>();
 
   // Why is this not allowed?
   ref.current = "Hello";
