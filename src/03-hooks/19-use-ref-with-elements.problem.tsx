25716bd897514b590d04f7a601dcbff10561f7e8
Sam <lenry101@gmail.com>
2023-09-17 23:33:11 +0100

    exercise 19

--- src/03-hooks/19-use-ref-with-elements.problem.tsx
+++ src/03-hooks/19-use-ref-with-elements.problem.tsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 
+// looking at the useRef type it has function overloads: function useRef<T>(initialValue: T|null): RefObject<T>;
+// it expects the initial value to be null or undefined
 export const Component = () => {
-  const ref = useRef<HTMLDivElement>();
-
+  const ref = useRef<HTMLDivElement>(null);
+  // hover over this div to see what the type is
   return <div ref={ref} />;
 };
