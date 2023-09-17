a49ecf1e55691d7dfe9f94e2c2bff59db45b5c1a
Sam <lenry101@gmail.com>
2023-09-17 23:59:21 +0100

    exercise 21

--- src/03-hooks/21-use-reducer.problem.ts
+++ src/03-hooks/21-use-reducer.problem.ts
@@ -1,7 +1,24 @@
 import { useReducer } from "react";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const reducer = (state: unknown, action: unknown) => {
+// our reducer state type = same as initial state on the useReducer below
+type ReducerState = {
+  count: number;
+};
+
+// our reducer actions based on the schemas as the end of the file when used with dispatch
+type ReducerAction =
+  | {
+  type: "add";
+  add: number;
+}
+  | {
+  type: "subtract";
+  subtract: number;
+};
+
+// assign the types to the inputs on the reducer function that is passed to useReducer
+const reducer = (state: ReducerState, action: ReducerAction) => {
   switch (action.type) {
     case "add":
       return { count: state.count + action.add };
